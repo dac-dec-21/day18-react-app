e69ff1f9133fd6e423bc891520e6e28e37792596
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-03 11:26:15 +0530

    react. website.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 export default function App() {
+  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
   return (
     <div className="container-fluid">
-      <div className="row bg-dark text-light p-3">
+      <div className="row bg-dark text-light p-3 sticky-top">
         <div className="col h3 ">React</div>
       </div>
 
-      <div className="row">
-        <div className="col-12 bg-secondary text-light p-3">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eos
-          fugiat recusandae accusamus? Illum odit ipsa rem labore officiis,
-          fugiat dignissimos praesentium doloribus delectus similique soluta
-          deserunt! Debitis, expedita facere!
+      {list.map((item) => (
+        <div className="row">
+          <div className="col-12 bg-secondary text-light p-3 mb-1">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+            eos fugiat recusandae accusamus? Illum odit ipsa rem labore
+            officiis, fugiat dignissimos praesentium doloribus delectus
+            similique soluta deserunt! Debitis, expedita facere!
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
