a19e371b6179e346396c726486fa35bb453efe51
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-03 11:16:24 +0530

    inline style.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import "./App.css";
-
 /**
- * class => is a reserved keywrod in JavaScript.
- * class => className
- * @returns
+ * style = ""
+ * style="background-color:red"
+ *
+ * RULE
+ * style = {{}}
+ * style = {{backgroundColor: "red"}}
+ *
+ * 1. First Two Letter Css Property are now Converted into camelCase proprty.
+ * 2. The value of the property must be in "DOUBLE QOUTE"
  */
 export default function App() {
   return (
     <div>
-      <div className="beautify">Hello World</div>
+      <div style={{ color: "white", backgroundColor: "royalblue" }}>
+        Hello World
+      </div>
     </div>
   );
 }
