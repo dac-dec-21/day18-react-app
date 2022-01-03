0488a15f85fd7079903468bd25ad416bb3587138
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-03 11:21:48 +0530

    b5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-/**
- * style = ""
- * style="background-color:red"
- *
- * RULE
- * style = {{}}
- * style = {{backgroundColor: "red"}}
- *
- * 1. First Two Letter Css Property are now Converted into camelCase proprty.
- * 2. The value of the property must be in "DOUBLE QOUTE"
- */
 export default function App() {
   return (
-    <div>
-      <div style={{ color: "white", backgroundColor: "royalblue" }}>
-        Hello World
+    <div className="container-fluid">
+      <div className="row bg-dark text-light p-3">
+        <div className="col h3 ">React</div>
+      </div>
+
+      <div className="row">
+        <div className="col-12 bg-secondary text-light p-3">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eos
+          fugiat recusandae accusamus? Illum odit ipsa rem labore officiis,
+          fugiat dignissimos praesentium doloribus delectus similique soluta
+          deserunt! Debitis, expedita facere!
+        </div>
       </div>
     </div>
   );
